b288675fdc058a996a7beebffe66821e73c385a6
weicheng lee <z82111712@gmail.com>
2022-01-24 15:00:28 -0800

    have form route to add new product

---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductForm from './ProductForm';
+
+const Main = () => {
+    return (
+        <div>
+            <ProductForm/>
+        </div>
+    )
+}
+export default Main;
---
+++ src/Components/ProductForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ProductForm = () => {
+    const [product, setProduct] = useState({
+        title: "",
+        price: "",
+        description: ""
+    })
+
+    const onSubmitHandler = e => {
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/products/new", {
+            title: product.title,
+            price: product.price,
+            description: product.description
+        }).then(res => console.log(res))
+            .catch(err => console.log({error: err}))
+    }
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>Title</label>
+                <input type="text" onChange={e => setProduct({ ...product, title: e.target.value })} />
+            </p>
+            <p>
+                <label>Price</label>
+                <input type="text" onChange={e => setProduct({ ...product, price: e.target.value })} />
+            </p>
+            <p>
+                <label>Description</label>
+                <input type="text" onChange={e => setProduct({ ...product, description: e.target.value })} />
+            </p>
+            <button>Add</button>
+        </form>
+    );
+};
+
+export default ProductForm;
